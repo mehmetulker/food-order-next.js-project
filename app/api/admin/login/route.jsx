6a7372a9517903b40251c72cf6a2de59da4776b8
mehmet ülker <mehmet-ulker@hotmail.com>
2025-09-18 15:33:58 +0300

    refactor(Login): remove commented code for cleaner implementation

--- app/api/admin/login/route.jsx
+++ app/api/admin/login/route.jsx
@@ -4,10 +4,6 @@ import jwt from "jsonwebtoken";
 export async function POST(req) {
   const body = await req.json();
   const { email, password } = body;
-
-  //console.log("Admin", body);
-
-  // JWT Token oluştur
   const token = jwt.sign(
     { email: process.env.ADMIN_USERNAME },
     process.env.JWT_SECRET,
@@ -19,20 +15,15 @@ export async function POST(req) {
       email === process.env.ADMIN_USERNAME &&
       password === process.env.ADMIN_PASSWORD
     ) {
-      // Önce cevap oluştur
       const response = NextResponse.json(
         { message: "Login başarılı" },
         { status: 200 }
       );
-
-      //console.log("Admin Login ", token);
-
-      // Sonra cookie ayarla
       response.cookies.set("token", token, {
         httpOnly: true,
         secure: true,
         path: "/",
-        maxAge: 60 * 60, // 1 saat
+        maxAge: 60 * 60,
       });
 
       return response;
